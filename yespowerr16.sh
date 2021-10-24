e07ada4f72f9717fda212b4eb0196f654cf48281
harkandon <92619165+harkandon@users.noreply.github.com>
2021-10-24 15:00:32 +0700

    Create yespowerr16.sh

---
+++ yespowerr16.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a yespowerr16 -o stratum+tcp://yespowerR16.na.mine.zpool.ca:6534 -u DK6MFYcQV2gVEku8PHfHiqtAWDPfbHnjf8.DEGE -t 8
